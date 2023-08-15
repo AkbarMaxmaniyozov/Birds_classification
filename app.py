ddf835273033a477adb565c20484f4853a5fada4
Akbar <120322124+AkbarMaxmaniyozov@users.noreply.github.com>
2023-08-16 00:35:00 +0500

    app.py

--- app.py
+++ app.py
@@ -18,21 +18,20 @@ if file:
         st.subheader("Faqat qushlar rasmini klassifikatsiya qilib beradi. Iltimos qushlar rasmini yuklang!")
         st.image(file, width=224)
     else:
-        if file:
-            st.image(file, width=224)
-            #PIL convert
-            img = PILImage.create(file)
+        st.image(file, width=224)
+        #PIL convert
+        img = PILImage.create(file)
 
-            #Modelni yuklash
-            model2 = load_learner("bird_model.pkl")
+        #Modelni yuklash
+        model2 = load_learner("bird_model.pkl")
 
-            #prediction
-            pred, pred_id, probs = model2.predict(img)
-            st.success(pred)
-            st.info(f"Ehtimollik: {probs[pred_id]:.1%}")
+        #prediction
+        pred, pred_id, probs = model2.predict(img)
+        st.success(pred)
+        st.info(f"Ehtimollik: {probs[pred_id]:.1%}")
 
-            #plotting
-            fig = px.bar(x=probs*100, 
-                        y=model2.dls.vocab,
-                        title="Klasslar bo'yicha ehtimollik")
-            st.plotly_chart(fig)
+        #plotting
+        fig = px.bar(x=probs*100, 
+                    y=model2.dls.vocab,
+                    title="Klasslar bo'yicha ehtimollik")
+        st.plotly_chart(fig)
